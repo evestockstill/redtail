283ded323c135c355f7d7c64c67d4a7795022659
Eve Stockstill <evestockstillpdx@gmail.com>
2020-06-23 00:05:15 -0700

    stashing

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Nav.scss';
+
+const Nav = () => {
+  return (
+    <>
+      <nav>
+        <h1>hello eve</h1>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,68 @@
+// const HtmlPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const Dotenv = require('dotenv-webpack');
+// const CopyPlugin = require('copy-webpack-plugin');
+
+// // eslint-disable-next-line
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'bundle.[hash].js',
+//     publicPath: '/'
+//   },
+//   devServer: {
+//     port: 7891,
+//     historyApiFallback: true
+//   },
+//   plugins: [
+//     new HtmlPlugin({ template: './src/index.html' }),
+//     new CleanWebpackPlugin(),
+//     new Dotenv({
+//       systemvars: true
+//     }),
+//     new CopyPlugin({
+//       patterns: [
+//         { from: 'public' },
+//       ]
+//     })
+//   ],
+//   resolve: {
+//     extensions: ['.js', '.jsx']
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             cacheDirectory: true
+//           }
+//         }
+//       },
+//       {
+//        test: /\.s[ac]ss$/i,
+//         use: [
+//           // Creates `style` nodes from JS strings
+//           'style-loader',
+//           // Translates CSS into CommonJS
+//           'css-loader',
+//           // Compiles Sass to CSS
+//           'sass-loader'
+//         ]
+//       },
+//       {
+//         test: /\.(jpeg|jpg|png|svg)$/,
+//         use: {
+//           loader: 'url-loader',
+//           options: { limit: 1000 },
+//         },
+//       }
+//     ]
+//   }
+// };
+
 const HtmlPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
@@ -8,66 +73,54 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.[hash].js',
-    publicPath: '/'
+    publicPath: '/',
   },
   devServer: {
     port: 7891,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlPlugin({ template: './src/index.html' }),
     new CleanWebpackPlugin(),
     new Dotenv({
-      systemvars: true
+      systemvars: true,
     }),
-    new CopyPlugin({
-      patterns: [
-        { from: 'public' },
-      ]
-    })
+    new CopyPlugin([{ from: 'public' }]),
   ],
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(mp4|webm|jpeg|mov)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[contenthash].[ext]',
+            outputPath: 'assets/videos/',
+            publicPath: 'assets/videos/',
+          },
+        },
+      },
+
+      {
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            cacheDirectory: true
-          }
-        }
+            cacheDirectory: true,
+          },
+        },
       },
       {
-        test: /\.css$/,
+        test: /\.s[ac]ss$/i,
         use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              modules: true,
-              importLoaders: 1
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              plugins: [
-                require('postcss-import')(),
-                require('autoprefixer')(),
-                require('postcss-nested')(),
-                require('postcss-simple-vars')()
-              ]
-            }
-          }
-        ]
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       },
       {
         test: /\.(jpeg|jpg|png|svg)$/,
@@ -75,7 +128,7 @@ module.exports = {
           loader: 'url-loader',
           options: { limit: 1000 },
         },
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
