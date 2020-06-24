aa0fb60f30c849dc410bf7382be1a52d59a5b736
Jose <j.a.ojeda@live.com>
2020-06-24 14:38:31 -0700

    created data.js added dummy data

---
+++ data/data.js
@@ -0,0 +1,9 @@
+export const dummyData = [
+  {
+    title: 'Pre-Foreclosure',
+    img: 'http://placekitten.com/200/300',
+    text: 'Si osculantur puer tuus aut uxorem tuam, osculum, non dico quod omnia quae sunt hominis, et sic non tangetur, si aut ex eis moriatur.',
+    link: 'Set up an appointment to find out more'
+  }
+]
+
