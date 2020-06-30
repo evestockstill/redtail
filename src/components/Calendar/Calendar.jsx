8596975925a8f088008f519bd4cf57d7aa0badd6
Jose <j.a.ojeda@live.com>
2020-06-30 12:49:51 -0700

    created Calendar.jsx and got it to render

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+
+const Calendar = () => {
+
+  useEffect(() => {
+
+    const script = document.createElement('script');
+    //<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('src', 'https://assets.calendly.com/assets/external/widget.js');
+    script.async = true;
+
+    const section = document.getElementById('calendar-container');
+    section.appendChild(script);
+
+  }, []);
+
+
+  return (
+    <section id='calendar-container'>
+      <div className="calendly-inline-widget" data-url="https://calendly.com/j-a-ojeda/30min" style={{ minWidth:'320px', height:'630px' }}></div>
+    </section>
+  );
+};
+
+export default Calendar;
