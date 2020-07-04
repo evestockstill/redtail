82b1939bb8ab0e00ca2640e2927b02858c9e6e56
Eve Stockstill <evestockstillpdx@gmail.com>
2020-07-03 23:19:25 -0700

    scroll on picture working

--- src/components/InfoSectionTest/InfoSectionTest.jsx
+++ src/components/InfoSectionTest/InfoSectionTest.jsx
@@ -1,38 +1,63 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import oldhome from '../../assets/istockUglyHome.jpg';
-import styles from './InfoSectionTest.css';
+import React, { useRef, useEffect } from 'react';
+import oldHome from '../../assets/istockUglyHome.jpg';
+import styles from './TestTwo.css';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
-const InfoSectionTest = ({ title, img, text, link }) => {
+const TestTwo = ({ title, img, text, link }) => {
+  const pContent = useRef(null);
+  const pImage = useRef(null);
+  const pSection = useRef(null);
+  useEffect(() => {
+    gsap.to(pContent.current, {
+      yPercent: -90,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: pSection.current,
+
+        scrub: true,
+      },
+    });
+
+    gsap.to(pImage.current, {
+      yPercent: 50,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: pSection.current,
+
+        scrub: true,
+      },
+    }),
+      [pContent, pImage];
+  });
+
   return (
-    //we may need to change the a tag based on how scrolling will work with greensock
     <>
-      <div className={styles.main_container}>
-        <div className={styles.info_container}>
-          <div className={styles.img_container}>
-            <h2>
-              <span className={styles.span}>Old Homes</span>
-            </h2>
-            <img src={oldhome} className={styles.mainImg} />
-            
-
-            <div className={styles.para}>
-              <p>
-                <span className={styles.pspan}>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
-                  dolore expedita corrupti qui dolorem, consequuntur porro
-                  nostrum at? Omnis nam minus aliquid exercitationem aspernatur
-                  a, numquam porro laudantium blanditiis inventore accusamus
-                  mollitia voluptate pariatur voluptatem ex nemo ad facere ut
-                
-                  <a>{link}</a>
-                </span>
+      <div className={styles.greenSock_container}>
+        <section ref={pSection} className={styles.pSection}>
+          <div className={styles.pContainer}>
+            <div ref={pContent} className={styles.pContent}>
+              <h2 className={styles.title}>Old Home</h2>
+              <p className={styles.paragraph}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam
+                laboriosam quibusdam tempora et vitae reiciendis rem amet
+                accusamus at ipsum. Suscipit voluptates animi eaque? Placeat
+                earum, amet harum quas laudantium debitis ratione consequuntur
+                nesciunt nostrum voluptatum officiis nulla quidem dolores
+                corporis, voluptate aperiam officia, velit quaerat impedit
+                porro! Quidem, at?
               </p>
+
+              <a href='https:/evestockstill.com'>set appointment</a>
             </div>
           </div>
-        </div>
+
+          <img ref={pImage} className={styles.pImage} src={oldHome}></img>
+        </section>
       </div>
     </>
   );
 };
-export default InfoSectionTest;
+export default TestTwo;
