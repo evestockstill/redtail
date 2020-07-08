13d34282262978b9076e8cce112f3787ef83e018
Jose <j.a.ojeda@live.com>
2020-07-08 12:58:13 -0700

    updated app with InfoSectionList

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import NavBar from '../nav/NavBar';
-import InfoSection from '../InfoSection/InfoSection';
-import TlcHomes from '../InfoSectionTest/TlcHomes';
-import Probate from '../InfoSectionTest/Probate';
-import PreForeclosure from '../InfoSectionTest/PreForeclosure';
 import Blank from '../InfoSectionTest/Blank';
-import PrivateMoney from '../InfoSectionTest/PrivateMoney';
-import Refinance from '../InfoSectionTest/Refinance';
+import { sectionsData } from '../../../data/data';
+import InfoSectionList from '../InfoSection/InfoSectionList';
 
 export default function App() {
   return (
     <>
       <NavBar />
       <Blank />
-      <PreForeclosure />
-      <Probate />
-      <TlcHomes/>
-      <Refinance />
-      <PrivateMoney />
+      <InfoSectionList sections={sectionsData} />
     </>
   );
 }
