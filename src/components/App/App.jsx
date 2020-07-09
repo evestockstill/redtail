befe01551bd78c4442b7d91aa4a7a2885bdd2281
Jose <j.a.ojeda@live.com>
2020-07-09 09:31:07 -0700

    added Calndar to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import NavBar from '../nav/NavBar';
 import Blank from '../InfoSectionTest/Blank';
 import { sectionsData } from '../../../data/data';
 import InfoSectionList from '../InfoSection/InfoSectionList';
+import Calendar from '../Calendar/Calendar';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <NavBar />
       <Blank />
       <InfoSectionList sections={sectionsData} />
+      <Calendar />
     </>
   );
 }
